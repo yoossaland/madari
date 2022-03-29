1bee5f58a42c319df004fb4293f44ed5d598fa71
Harshad Sharma <harshad@sharma.io>
2022-03-29 20:54:59 +0530

    Fix tests, update home screen

--- test/madari_web/controllers/page_controller_test.exs
+++ test/madari_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MadariWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Madari"
   end
 end
