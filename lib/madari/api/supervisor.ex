404b84da601e51b8e260d2ca2638695a9482a16b
Harshad Sharma <harshad@sharma.io>
2022-04-08 08:07:48 +0530

    Disable dmesg logs for now

--- lib/madari/api/supervisor.ex
+++ lib/madari/api/supervisor.ex
@@ -13,7 +13,7 @@ defmodule Madari.Api.Supervisor do
       worker(Madari.Api.Logs.Syslog, [[name: Madari.Api.Logs.Syslog]]),
       worker(Madari.Api.Logs.StreamFile, [%{name: :messages, topic: "syslog", logfile: "/var/log/messages"}]),
       worker(Madari.Api.Logs.StreamFile, [%{name: :syncthing, topic: "syslog", logfile: "/var/log/syncthing.log"}], id: :syncthing),
-      worker(Madari.Api.Logs.StreamFile, [%{name: :dmesg, topic: "syslog", logfile: "/var/log/dmesg.today"}], id: :dmesg),
+      # worker(Madari.Api.Logs.StreamFile, [%{name: :dmesg, topic: "syslog", logfile: "/var/log/dmesg.today"}], id: :dmesg),
     ]
     supervise(children, strategy: :one_for_one)
   end
